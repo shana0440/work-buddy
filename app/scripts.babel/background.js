2078c4d69121717867b811b56fb4a3667ceb0fb6
kw <shana0440@gmail.com>
2017-05-11 00:09:47 +0800

    add badgeText on disable

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -13,12 +13,16 @@ chrome.runtime.onInstalled.addListener(details => {
 });
 
 chrome.browserAction.onClicked.addListener(tab => {
-  console.log(tab);
   disableApp = !disableApp;
   chrome.storage.sync.set({
     disable: disableApp
   });
-  chrome.tabs.query({}, function (tabs) {
+  if (disableApp) {
+    chrome.browserAction.setBadgeText({text: 'disable'});
+  } else {
+    chrome.browserAction.setBadgeText({text: ''});
+  }
+  chrome.tabs.query({}, tabs => {
     for (var tab of tabs) {
       chrome.tabs.sendMessage(tab.id, {
         directive: disableApp ? 'disable-app' : 'enable-app'
